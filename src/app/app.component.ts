38c40767d1d60e2ca51dfa8e9d45f81af2493467
charles <charlesrudisel@gmail.com>
2021-01-29 16:18:52 -0500

    for the sake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pos-app';
+
+  transactions = []
+  balance = 0;
+
+
+  submit(event: any) {
+    //this.member_ID = event.target.value
+    this.transactions.push(event.target.value)
+    this.balance += Number(event.target.value)
+
+  }
+
+   clear(){
+     
+    this.transactions = []
+    this.balance = 0;
+   }
+
+
 }
