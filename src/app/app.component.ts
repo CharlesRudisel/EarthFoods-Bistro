2ec4b32e7737e07827ed09785ed16e910ed81d8c
charles <charlesrudisel@gmail.com>
2021-02-04 16:29:12 -0500

    for the sake

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,32 @@ export class AppComponent {
   public trans: Transaction;
 
   transactions = []
+  cat_name = 'EarthFoods Signature Dishes';
   balance = 0;
   checkout = []
-  catergory = []
+  
+
+  main = [
+
+    new Item(9, "Quinoa Enchilada Bake"),
+    new Item(8, "Parmesan and Spinach Orzo"),
+    new Item(11, "Herbed Cranberry Chicken"),
+    new Item(9, "French Onion Chicken"),
+    new Item(5.5, "Mini Buffalo Chicken Crunchwraps"),
+    new Item(5, "Cold Spicy Peanut Sesame Noodles"),
+    new Item(11, "Cajun Chicken Pasta"),
+    new Item(4.5, "Basil Beef Stir Fry"),
+    new Item(9, "Drunken Noodles with Tofu"),
+    new Item(7, "Korean Beef"),
+    new Item(9, "Honey Jalapeño Grilled Chicken Drumsticks"),
+    new Item(8.25, "Bangin’ Mac and Cheese"),
+    new Item(7.5, "Bulgogi Kimchi Burger"),
+    new Item(12.75, "Eggplant Parmesan Sandwich"),
+    new Item(12, "Creamy Steak Fettuccine"),
+    new Item(13.25, "Reverse Sear Steak"),
+    new Item(10.25, "Juicy Steak-and-Tomato Salad"),
+    new Item(9.5, "Pan-Roasted Salmon Salad"),
+  ]
   breakfast = [
 
     new Item(9, "French Toast"),
@@ -52,7 +75,95 @@ export class AppComponent {
     new Item(7, "lobster bisque"),
 
   ]
+  pizza = [
 
+    new Item(9, "French Toast"),
+    new Item(8, "Huevos Rancheros"),
+    new Item(11, "Brisket Breakfast"),
+    new Item(9, "Southwest Tofu Scramble"),
+    new Item(5.5, "Buttermilk Pancakes"),
+    new Item(5, "Buscuits and Gravy"),
+    new Item(11, "Fish and Grits"),
+    new Item(4.5, "Granola Oatmeal"),
+    new Item(9, "Bluebird Burrito"),
+    new Item(7, "Bionic Breakfast"),
+    new Item(9, "Early Bird Special"),
+    new Item(8.25, "Today's Omelette"),
+    new Item(7.5, "Floridan French Toast"),
+    new Item(12.75, "Full English Breakfast"),
+    new Item(12, "Lemon Riccotta French Toast"),
+    new Item(13.25, "Smoked Salmon Omelette"),
+    new Item(10.25, "The All-American Breakfast"),
+    new Item(9.5, "Breakfast Bowl"),
+  ]
+
+  lunch = [
+
+    new Item(9, "Tomato Basil Soup"),
+    new Item(8, "Potato & Cheese Soup"),
+    new Item(11, "Asian Chili"),
+    new Item(9, "Bacon Corn Chowder"),
+    new Item(5.5, "Creamy Chicken Enchilada Soup"),
+    new Item(5, "Loaded Baked Potato Soup"),
+    new Item(11, "Manhattan Clam Chowder"),
+    new Item(4.5, "Minestrone"),
+    new Item(9, "Zesty Tomato Cheese Soup"),
+    new Item(7, "lobster bisque"),
+
+  ]
+
+  drinks = [
+
+    new Item(9, "French Toast"),
+    new Item(8, "Huevos Rancheros"),
+    new Item(11, "Brisket Breakfast"),
+    new Item(9, "Southwest Tofu Scramble"),
+    new Item(5.5, "Buttermilk Pancakes"),
+    new Item(5, "Buscuits and Gravy"),
+    new Item(11, "Fish and Grits"),
+    new Item(4.5, "Granola Oatmeal"),
+    new Item(9, "Bluebird Burrito"),
+    new Item(7, "Bionic Breakfast"),
+    new Item(9, "Early Bird Special"),
+    new Item(8.25, "Today's Omelette"),
+    new Item(7.5, "Floridan French Toast"),
+    new Item(12.75, "Full English Breakfast"),
+    new Item(12, "Lemon Riccotta French Toast"),
+    new Item(13.25, "Smoked Salmon Omelette"),
+    new Item(10.25, "The All-American Breakfast"),
+    new Item(9.5, "Breakfast Bowl"),
+  ]
+
+  desert = [
+
+    new Item(9, "Tomato Basil Soup"),
+    new Item(8, "Potato & Cheese Soup"),
+    new Item(11, "Asian Chili"),
+    new Item(9, "Bacon Corn Chowder"),
+    new Item(5.5, "Creamy Chicken Enchilada Soup"),
+    new Item(5, "Loaded Baked Potato Soup"),
+    new Item(11, "Manhattan Clam Chowder"),
+    new Item(4.5, "Minestrone"),
+    new Item(9, "Zesty Tomato Cheese Soup"),
+    new Item(7, "lobster bisque"),
+
+  ] 
+  beer = [
+
+    new Item(9, "Tomato Basil Soup"),
+    new Item(8, "Potato & Cheese Soup"),
+    new Item(11, "Asian Chili"),
+    new Item(9, "Bacon Corn Chowder"),
+    new Item(5.5, "Creamy Chicken Enchilada Soup"),
+    new Item(5, "Loaded Baked Potato Soup"),
+    new Item(11, "Manhattan Clam Chowder"),
+    new Item(4.5, "Minestrone"),
+    new Item(9, "Zesty Tomato Cheese Soup"),
+    new Item(7, "lobster bisque"),
+
+  ] 
+
+  catergory = this.main
 
   submit(event: any) {
 
@@ -84,28 +195,36 @@ export class AppComponent {
   catergories(event: any) {
     switch (event.target.value) {
       case "mains":
-        this.catergory = this.soups;
+        this.catergory = this.main;
+        this.cat_name = 'EarthFoods Signature Dishes'
         break;
       case "breakfast":
         this.catergory = this.breakfast;
+        this.cat_name = 'Breakfast Menu'
         break;
       case "soups":
         this.catergory = this.soups;
+        this.cat_name = 'Soups'
         break;
-      case "mains":
-        this.catergory = this.soups;
+      case "pizza":
+        this.catergory = this.pizza;
+        this.cat_name = 'Pizza'
         break;
-      case "mains":
-        this.catergory = this.soups;
+      case "lunch":
+        this.catergory = this.lunch;
+        this.cat_name = 'Lunch Menu'
         break;
-      case "mains":
-        this.catergory = this.soups;
+      case "drinks":
+        this.catergory = this.drinks;
+        this.cat_name = 'Soft Drinks'
         break;
-      case "mains":
-        this.catergory = this.soups;
+      case "desert":
+        this.catergory = this.desert;
+        this.cat_name = 'Deserts'
         break;
-      case "mains":
-        this.catergory = this.soups;
+      case "beer":
+        this.catergory = this.beer;
+        this.cat_name = 'Beer Wine and Spirits'
         break;
     }
 
